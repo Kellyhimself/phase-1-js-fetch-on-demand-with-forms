3d4afea1a5f7b3108fb467f76e5d66e3abec1c88
Kitui Kelly <kituikelly@gmail.com>
2024-04-04 23:02:30 +0300

    completes lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = document.querySelector("input#searchByID").value; //e.target.children[1].value
 
-document.addEventListener('DOMContentLoaded', init);
+    fetch(`http://localhost:3000/movies/${input}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
